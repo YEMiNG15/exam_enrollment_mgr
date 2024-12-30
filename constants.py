2ec8a3375716dc2fe512f4fde4162704a94b9e09
Tianyu Zhang <dowdah@163.com>
2024-12-30 14:22:04 +0800

    1)Split main.py to avoid main.py being too bloated;2)Add search and list function for objs;3)Make prep. for add function for objs

---
+++ constants.py
@@ -0,0 +1,23 @@
+import os
+
+
+# 数据库文件路径默认为脚本所在目录下的database.sqlite
+DATABASE_PATH = os.path.join(os.path.dirname(__file__), 'database.sqlite')
+DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
+DATETIME_STR_FORMAT = "%Y-%m-%d_%H:%M:%S"
+DATE_STR_FORMAT = "%Y-%m-%d"
+TRANSLATION = {
+    'name': '姓名',
+    'phone_number': '电话号码',
+    'registered_date': '注册日期',
+    'title': '考试名称',
+    'date': '考试日期',
+    'location': '考试地点',
+    'max_candidates': '最大考生数',
+    'registration_date': '报名日期',
+    'candidate_id': '考生ID',
+    'exam_id': '考试ID',
+    'Candidate': '考生',
+    'Exam': '考试',
+    'Registration': '报名记录'
+}
