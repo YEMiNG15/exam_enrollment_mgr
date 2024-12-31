3955c8952e661c629d924949c8c78513db7f5296
YEMiNG15 <1665734951@qq.com>
2024-12-31 11:20:29 +0800

    Update Function (#5)
    * Update function
    
    * Add input_text function
    
    * Update comments of input_text function
    
    ---------
    
    Co-authored-by: Tianyu Zhang <dowdah@163.com>

--- models.py
+++ models.py
@@ -12,6 +12,7 @@ class Candidate(Base):
     ALLOWED_SEARCH_FIELDS = ['id', 'name', 'phone_number'] # 允许的搜索字段
     REQUIRED_FIELDS = ['name', 'phone_number'] # 创建时必须提供的字段
     OPTIONAL_FIELDS = ['registered_date_str'] # 创建时可选的字段
+    ALLOWED_MODIFY_FIELDS = ['name', 'phone_number', 'registered_date_str'] # 允许修改的字段
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(100), nullable=False)
     phone_number = Column(String(20), unique=True, nullable=False)
@@ -39,6 +40,7 @@ class Exam(Base):
     ALLOWED_SEARCH_FIELDS = ['id', 'title', 'location'] # 允许的搜索字段
     REQUIRED_FIELDS = ['title', 'location'] # 创建时必须提供的字段
     OPTIONAL_FIELDS = ['date_str'] # 创建时可选的字段
+    ALLOWED_MODIFY_FIELDS = ['title', 'location', 'date_str'] # 允许修改的字段
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String(200), nullable=False)
     date = Column(Date, nullable=False, default=datetime.date.today)
@@ -66,6 +68,7 @@ class Registration(Base):
     ALLOWED_SEARCH_FIELDS = ['id', 'candidate_id', 'exam_id'] # 允许的搜索字段
     REQUIRED_FIELDS = ['candidate_id', 'exam_id'] # 创建时必须提供的字段
     OPTIONAL_FIELDS = ['registration_date_str'] # 创建时可选的字段
+    ALLOWED_MODIFY_FIELDS = ['registration_date_str'] # 允许修改的字段
     id = Column(Integer, primary_key=True, autoincrement=True)
     candidate_id = Column(Integer, ForeignKey('candidates.id'), nullable=False)
     exam_id = Column(Integer, ForeignKey('exams.id'), nullable=False)
